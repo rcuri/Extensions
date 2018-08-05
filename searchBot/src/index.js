1d272d3ed34c58fc7455b812293c0a1f200dfc76
rcuri <rcuriel@g.ucla.edu>
2018-08-04 18:34:57 -0700

    added twilio text message notification for new job

---
+++ searchBot/src/index.js
@@ -0,0 +1,72 @@
+//require('dotenv/config')
+
+const rp = require("request-promise");
+const checksum = require("checksum");
+const co = require("cheerio");
+
+acc_SID = 'private';
+auth_token = 'private';
+const client = require('twilio')(acc_SID, auth_token)
+
+const url = `https://www.indeed.com/jobs?q=Software+engineer+intern&l=California&sort=date`;
+
+// empty var outside function so we can save its value
+let hash = "";
+
+function checkURL(siteToCheck) {
+  return rp(siteToCheck)
+  .then(HTMLresponse => {
+    const $ = co.load(HTMLresponse);
+    let jobString = "";
+
+    $(".row.result.clickcard").attr("data-tn-component", "organicJob").each((i, element) => {
+      jobString += `${element.attribs["id"]}`;
+    });
+
+    if(hash === '') {
+      console.log('Making initial fetch...')
+      hash = checksum(jobString);
+    }
+
+    if(hash !== checksum(jobString)) {
+      console.log(checksum(jobString));
+      hash = checksum(jobString)
+      return true;
+    }
+    return false;
+  })
+  .catch(err => {
+    console.log(`Could not complete fetch of ${url}: ${err}`);
+  });
+}
+
+function SMS({
+  body,
+  to,
+  from
+}) {
+  client.messages
+    .create({
+      body,
+      to,
+      from
+    })
+    .then(() => {
+      console.log(`Success! Message has been sent to ${to}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+setInterval(async() => {
+  if(await checkURL(url)) {
+    console.log('Found a change! Sending text.')
+    SMS({
+      body: `There is a new add at ${url}`,
+      to: "+18185777925",
+      from: "+18057741640"
+    })
+  }
+  console.log('No new ads.');
+}, 10000);
